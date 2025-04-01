49c3a69b7ef6df616480d2cf9674065085283065
NJSuchar <NJSuchar@gmail.com>
2025-04-01 01:00:11 -0400

    Create account.sql

---
+++ sql/account/account.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Users (
+    userID INT AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for each user
+    username VARCHAR(50) NOT NULL,         -- Username (up to 50 characters)
+    password VARCHAR(255) NOT NULL,        -- Password (hashed, up to 255 characters)
+    isAdmin BOOLEAN NOT NULL DEFAULT 0     -- Boolean to indicate admin status (default is false)
+);
